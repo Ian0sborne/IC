9b57d51444cfc377249ccaf1f909cb60ff5eb496
Ian0sborne <ian.osborne@student.manchester.ac.uk>
2025-11-06 16:48:29 +0000

    improve git function

--- invisible_cities/cities/components.py
+++ invisible_cities/cities/components.py
@@ -227,6 +227,7 @@ def run_git_command(git_command: str):
 def add_git_info():
     git_info = dict(
         IC_tag        = run_git_command("git describe --tags --exact-match"),
+        closest_IC_tag = run_git_command("git describe --tags"),
         upstream_name = run_git_command("git rev-parse --abbrev-ref @{upstream}").split('/')[0],
         branch_name   = run_git_command("git branch --show-current"),
         commit_hash   = run_git_command("git rev-parse HEAD")
