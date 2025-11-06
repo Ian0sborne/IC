ad3223419d19d00182ce40577c525c1805b81e48
Ian0sborne <ian.osborne@student.manchester.ac.uk>
2025-11-06 21:29:19 +0000

    remove assert false from test

--- invisible_cities/cities/components_test.py
+++ invisible_cities/cities/components_test.py
@@ -553,7 +553,6 @@ def test_hits_corrector_valid_normalization_options( correction_map_filename
 
 @mark.skipif("os.environ['IS_GHA'] == 1", reason="this messes with git history so we only run it on GHA")
 def test_add_git_info():
-    assert False
     try:
         # keep a copy of the current branch
         current_branch = run_git_command("git branch --show-current")
